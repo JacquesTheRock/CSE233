fe8ef3f7bb05f0653331dc0ba70b87613858e28a
dcfreema <dcfreema@oakland.edu>
2016-11-14 16:07:56 -0500

    Add question #5

---
+++ question5.py
@@ -0,0 +1,18 @@
+# Group 3
+# Question 7.3
+# Donald Freeman
+
+import re
+
+userAcc = input('Enter you 7 digit user account: ')
+userAcc = str(userAcc)
+f = open('text5.txt', 'r')
+data = []
+for line in f:
+  newLine = re.sub('[\n]', '', line)
+  data.append(newLine)
+
+if any(userAcc in s for s in data):
+  print 'Your account is valid and exists.'
+else:
+  print 'Invalid account number.'
