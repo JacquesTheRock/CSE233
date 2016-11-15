825523f4d2238a84d7faf4095be7f7f4f7eae42a
JacquesTheRock <JacquesTheRock@users.noreply.github.com>
2016-11-15 01:47:38 -0500

    Add newline to input
    Added new line to input, should match now...

--- question5.py
+++ question5.py
@@ -1,24 +1,10 @@
 # Group 3
 # Question 7.5
 # Donald Freeman
-
-#import re
-#userAcc = input('Enter you 7 digit user account: ')
-#userAcc = str(userAcc)
-#f = open('text5.txt', 'r')
-#data = []
-#for line in f:
-#  newLine = re.sub('[\n]', '', line)
-#  data.append(newLine)
-#
-#if any(userAcc in s for s in data):
-#  print 'Your account is valid and exists.'
-#else:
-#  print 'Invalid account number.'
-
-f = open("text5.txt",'r')
+f = open("Accounts.txt",'r')
 acc = input("Please input an account")
-if acc in f:
+acc = acc + "\n"
+if acc in f.readlines():
 	print("Account found")
 else:
 	print("Account not found")
